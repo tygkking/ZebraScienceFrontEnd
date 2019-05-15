0e2b7453dfe9f9c220821f9b551b215840d97c11
longintlong <747099367@qq.com>
2019-05-15 15:40:31 +0800

    Search by enter

--- src/views/index.vue
+++ src/views/index.vue
@@ -40,7 +40,7 @@
 </style>
 <template>
     <div class="index">
-        <Button type="text" @click="modal1=true" style="margin-left: 89%;font-size: 15px;text-align: right">登陆</Button>
+        <Button type="text" @click="modal1=true" style="margin-left: 89%;font-size: 15px;text-align: right">登录</Button>
         <Modal v-model="modal1" title="登录" ok-text="登录" cancel-text="取消" @on-ok="login" @on-cancel="cancel">
             <p>用户名<input style="margin-left: 8px"/></p><br/>
             <p>密  码<input style="margin-left: 17px"/></p>
@@ -55,13 +55,13 @@
                 <h2>
                     <p>Welcome to Zebra Science!</p>
                 </h2>
-                <Input style="width: 50%; margin-left: 25%" v-model="search_content">
+                <Input style="width: 50%; margin-left: 25%" v-model="search_content" @keyup.enter.native="search">
                     <Select v-model="search_item" slot="prepend" style="width: 80px;background-color: #eeeeee;color: black">
                         <Option value="prof">专家</Option>
                         <Option value="paper">论文</Option>
                         <Option value="org">机构</Option>
                     </Select>
-                    <Button @click="search" slot="append" style="background-color:#57c5f7;color: white" icon="ios-search" ></Button>
+                    <Button  @click="search"  slot="append" style="background-color:#57c5f7;color: white" icon="ios-search" ></Button>
                 </Input>
             </Col>
         </Row>
@@ -156,6 +156,8 @@
                 this.$router.push({path: '/register'})
             },
             search() {
+                if(this.search_content == "")
+                    return;
                 if (this.search_item=='prof')
                     alert("专家搜索："+this.search_content);
                 else if (this.search_item=='paper')
