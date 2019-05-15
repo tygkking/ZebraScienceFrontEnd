aaa4304d85320a6c4dcdd915e3d170987412e61f
tygkking <tygkking@163.com>
2019-05-15 16:14:18 +0800

    search_detail.vue
    新增页首菜单栏和页尾标签

--- src/views/search_detail.vue
+++ src/views/search_detail.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
         <div class="top_xf">
+            <Menu mode="horizontal" :theme="theme1" active-name="1" style="width:100%; position: fixed">
+            <a :href="index_url" style="float:left">
+                <MenuItem name="1">
+                    <Icon type="ios-home" size="20"/>
+                    首页
+                </MenuItem>
+            </a>
+            <Submenu v-if="identity != 'visitor'" name="2" style="float:right">
+                <template slot="title">
+                    <Icon type="ios-contact" size="20"/>
+                    用户信息
+                </template>
+                <MenuItem name="2-1" @click.native="userpage()">个人主页</MenuItem>
+                <MenuItem name="2-2" @click.native="setting()">设置</MenuItem>
+                <MenuItem name="2-3" @click.native="logout()">退出登录</MenuItem>
+            </Submenu>
+            <MenuItem v-if="identity == 'visitor'" @click.native="modal1=true" name="3" style="float:right">
+                登录
+                <Modal v-model="modal1" title="登录" ok-text="登录" cancel-text="取消" @on-ok="login" @on-cancel="cancel">
+                    <p>用户名<input style="margin-left: 8px"/></p><br/>
+                    <p>密  码<input style="margin-left: 17px"/></p>
+                </Modal>
+            </MenuItem>
+            <a :href="register_url" style="float: right;">
+                <MenuItem v-if="identity == 'visitor'" :href="register_url" name="4">
+                    注册
+                </MenuItem>
+            </a>
+        </Menu>
             asd
         </div>
         <div class="sc_detail">
@@ -53,6 +82,12 @@
             <img src="/src/images/zebra.png" style="width: 300px;height: 300px">
             <p style="text-align: center;font-size: 30px">斑马学术<br>给你不一样的体验</p>
         </div>
+        <Layout id="layout">
+            <Footer class="layout-footer-center" style="background-color: #666666; color: #eeeeee;">
+                2019-2019 &copy; ZebraScience
+                <a href="http://www.baidu.com" style="margin-left: 20px; color: #eeeeee">联系我们</a>
+            </Footer>
+        </Layout>
     </div>
 </template>
 
@@ -62,6 +97,11 @@
         name: "search_paper",
         data() {
             return {
+                modal1: false,
+                index_url:'/',
+                register_url:'/register',
+                identity:'professor', //professor user visitor
+                theme1: 'primary',
                 search_results: [],
                 type: ''
             }
@@ -70,6 +110,21 @@
             this.getSearchDetails()
         },
         methods: {
+            login () {
+                this.$Message.info('login');
+            },
+            cancel () {
+                this.$Message.info('cancel');
+            },
+            userpage () {
+                alert("To user page")
+            },
+            setting () {
+                alert('To Setting page')
+            },
+            logout () {
+                alert('User Logout')
+            },
             getSearchDetails() {
                 this.search_results = this.$route.query.search_detail;
                 this.type = this.$route.query.search_type;
@@ -83,6 +138,14 @@
 </script>
 
 <style scoped>
+    #layout{
+        margin-top: 60px;
+        padding-top: 40px;
+        width: 100%;
+    }
+    .layout-footer-center{
+        text-align: center;
+    }
     .top_xf{
         background-color:#666666;
         width:100%;
