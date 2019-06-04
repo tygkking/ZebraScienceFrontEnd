cafe4750c0796d9c5dba90bc86ad947788d942b9
qi hu <424049382@qq.com>
2019-06-05 00:43:15 +0800

    联系我们页面加了图片

--- src/views/aboutUs.vue
+++ src/views/aboutUs.vue
@@ -2,12 +2,15 @@
     <div>
         <MenuBar></MenuBar>
         <div class="aboutUs" >
+            <img src="../images/zebra.png" style="height: 220px"><br>
+            <span style="font-size: 35px">ZebraScience</span><br>
+            <span style="color: #2db7f5">v1.0</span>
+            <br><br>
             <span style="font-size: 30px">团队介绍：</span><br>
             前端工程狮：<b>赵楠</b>、<b>何浩乾</b>、<b>胡琦</b><br>
             后端工程狮：<b>黎昆昌</b>、<b>仇善召</b>、<b>杜予同</b><br>
             数据库工程狮：<b>阙子烝</b>、<b>郭子溢</b><br>
             爬虫工程狮：<b>许志达</b>
-            <br>
             <br>
             <br>
             <span style="font-size: 30px">联系我们：</span><br>
@@ -34,6 +37,6 @@
         width: 100%;
         text-align: center;
         /* background-color: #666; */
-        padding-top: 20%;
+        padding-top: 8%;
     }
 </style>
